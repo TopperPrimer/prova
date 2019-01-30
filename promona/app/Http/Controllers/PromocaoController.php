ed68d32fcabe6f58e0563e54c2a3482477ca29cc
Leandro Alves <leandro.alves351@gmail.com>
2019-01-29 22:30:11 -0300

    rota alterada

--- promona/app/Http/Controllers/PromocaoController.php
+++ promona/app/Http/Controllers/PromocaoController.php
@@ -2,24 +2,33 @@
 
 class PromocaoController extends Controller{
 
+
+
     public function adicionarPromocao(){
     
+        
+    
+    
+    }
+
+    public function formularioPromocao(){
+
         return view('promocao.adicionar');
+
+    }
     
     
-        }
-    
-        public function listarPromocao(){
-    
-    
-            
-        }
-    
-        public function removerPromocao(){
+    public function listarPromocao(){
     
     
             
-        }
+    }
+    
+    public function removerPromocao(){
+    
+    
+            
+    }
 
     
 }

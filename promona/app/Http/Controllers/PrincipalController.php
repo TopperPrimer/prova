60ff6c6ebe0d7b347e6a423aa026de348edd82ca
Leandro Alves <leandro.alves351@gmail.com>
2019-01-31 17:55:09 -0300

    Retorna menu principal

---
+++ promona/app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,14 @@
+<?php namespace promona\Http\Controllers;
+
+class PrincipalController extends Controller
+{
+
+    public function principal(){
+
+
+        return view('layout.principal');
+
+    }
+
+
+}

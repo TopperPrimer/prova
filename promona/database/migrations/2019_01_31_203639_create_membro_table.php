927080f74456be6e47bec74dbac31ff9d6575d6d
Leandro Alves <leandro.alves351@gmail.com>
2019-01-31 17:56:06 -0300

    migration de membro sem relacionamento

---
+++ promona/database/migrations/2019_01_31_203639_create_membro_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membro', function (Blueprint $table) {
+            $table->intIncrements('id');
+            $table->string('primeiro_nome',200);
+            $table->string('segundo_nome',200);
+            $table->tinyInteger('ativo');
+            $table->string('cpf',11);
+            $table->dateTime('data_cadastro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('membro', function (Blueprint $table) {
+            Schema::dropIfExists('membro');
+        });
+    }
+}

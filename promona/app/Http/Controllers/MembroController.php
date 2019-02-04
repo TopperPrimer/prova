2f04563c6f42e83c4af17550a4f2e06d30c4e9da
Leandro Alves <leandro.alves351@gmail.com>
2019-02-04 01:34:35 -0300

    Controllers finalizados

--- promona/app/Http/Controllers/MembroController.php
+++ promona/app/Http/Controllers/MembroController.php
@@ -1,32 +1,74 @@
 <?php namespace promona\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
+use promona\Models\Membro;
+
+use Request;
+
 class MembroController extends Controller{
 
 
-    public function adicionarMembro(){
-    
+    public function adicionarMembro()
+    {
         
-    
-    
+        Membro::create(Request::all());
+
+        return redirect()
+        ->action ('MembroController@listarMembro');     
     }
 
-    public function formularioMembro(){
-
+    public function formularioMembro()
+    {
         return view('membro.adicionar');
 
     }
     
     
-    public function listarMembro(){
-    
-    
-            
+    public function listarMembro()
+    {
+        $membro = Membro::all();
+        
+        return view('membro.listar')->withmem($membro);       
     }
     
-    public function removerMembro(){
+    public function removerMembro($id){
     
-    
+        $membro = Membro::find($id);
+        $membro->delete();
+        
+        return redirect()
+        ->action ('MembroController@listarMembro');
             
+    }
+
+    public function editarMembro()
+    {
+        $id                    = Request::input('id');
+        $membro                = Membro::find($id);
+        $membro->id            = Request::input('id');
+        $membro->primeiro_nome = Request::input('primeiro_nome');
+        $membro->sobrenome     = Request::input('sobrenome');
+        $membro->cpf           = Request::input('cpf');
+        $membro->ativo         = Request::input('ativo'); 
+        $membro->save();
+        
+        return redirect()
+        ->action ('MembroController@listarMembro');
+
+    }
+    
+        
+
+
+    public function formularioEditarMembro($id)
+    {
+
+        $membro = Membro::find($id);
+
+        return view('membro.editar')->withm($membro);
+
+
     }
 
     
--- promona/app/Http/Controllers/PromocaoController.php
+++ promona/app/Http/Controllers/PromocaoController.php
@@ -1,11 +1,21 @@
 <?php namespace promona\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
+use promona\Models\Promocao;
+
+use Request;
+
 class PromocaoController extends Controller{
 
 
 
     public function adicionarPromocao(){
     
+        Promocao::create(Request::all());
+
+        return redirect()
+        ->action ('PromocaoController@listarPromocao');
         
     
     
@@ -20,14 +30,48 @@ class PromocaoController extends Controller{
     
     public function listarPromocao(){
     
-    
+        $promocao = Promocao::all();
+
+        return view('promocao.listar')->withpro($promocao);
             
     }
     
-    public function removerPromocao(){
+    public function removerPromocao($id){
     
-    
+        $promocao = Promocao::find($id);
+        $promocao->delete();
+        
+        return redirect()
+        ->action ('PromocaoController@listarPromocao');
             
+    }
+
+    public function editarPromocao()
+    {
+        $id                       = Request::input('id');
+        $promocao                 = Promocao::find($id);
+        $promocao->id             = Request::input('id');
+        $promocao->descricao      = Request::input('descricao');
+        $promocao->valor_desconto = Request::input('valor_desconto');
+        $promocao->percentual     = Request::input('percentual');
+        $promocao->ativo          = Request::input('ativo');
+        $promocao->save();
+        
+        return redirect()
+        ->action ('PromocaoController@listarPromocao');
+
+    }
+    
+        
+
+
+    public function formularioEditarPromocao($id){
+
+        $promocao = Promocao::find($id);
+
+        return view('promocao.editar')->withp($promocao);
+
+
     }
 
     
